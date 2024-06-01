57cebefd03c09cc38952ab71069a31a9fbd962b3
owo93 <Iztyle30@gmail.com>
2024-06-01 11:15:06 +0700

    new: week 3 assignment 1 practice 4

--- week-03/practice3.c
+++ week-03/practice3.c
@@ -7,4 +7,6 @@ int main() {
     printf("x: %d\ny: %.2f\nz: %.2f\n", x, y, z); // Print x, y and z with 2 decimal places
     float sum = x + y + z; // Add x and y and store the result in sum
     printf("The sum of %d, %.2f and %.2f is %.2f\n", x, y, z, sum); // Print the sum of x and y with 2 decimal places
+    
+    return 0;
 }
